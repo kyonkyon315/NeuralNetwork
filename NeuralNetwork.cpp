01e65dfea96660b32550706288dcca7670bea58a
Kyoya Sugiyama <k_sugiyama@defios.jp>
2025-03-07 10:12:16 +0900

    activate

--- NeuralNetwork.cpp
+++ NeuralNetwork.cpp
@@ -10,70 +10,89 @@ int main()
 {
 	std::string dataPath = "C:\\Users\\sugin\\source\\repos\\NeuralNetwork\\x64\\Debug\\MNIST_CSV";
 	MnistData<double> mnistData(dataPath);
-	std::random_device seed_gen;
-	std::default_random_engine engine(seed_gen());
-	std::normal_distribution<double> dist(static_cast<double>(0.), static_cast<double>(1.) / sqrt((double)100));
-	//std::cout<<dist(engine);
+	std::vector<std::pair<double*, size_t>> testData = mnistData.get_testData();
 	
-	const int n = 100000;
-	//make traindata
-	double** x = new double* [n];
-	int* y = new int[n];
-	for (int i = 0; i < n; i++) {
-		x[i] = new double[2];
-	}
-	for (int i = 0; i < n; i++) {
-		x[i][0] = (double)rand()/(double)RAND_MAX;
-		x[i][1] = (double)rand() / (double)RAND_MAX;
-		y[i] = (x[i][0] > x[i][1] ? 0 : 1);
-		//y[i] = 1 ;
-	}
-
-	int inputSize = 2;
-	int outputSize = 1;
 	double* input=nullptr;
 	double* output = nullptr;
 	double* d_input = nullptr;
 	double* d_output = nullptr;
 	
 	NN<double> nn;
-	nn.set_input(2, d_input);
-	nn.addLayer(100, "relu");
-	nn.addLayer(100, "relu");
-	nn.addLayer(2, "relu");
+	nn.set_input(784, d_input);
+	nn.addLayer(1000, "leakyRelu");
+	nn.addLayer(800, "leakyRelu");
+	nn.addLayer(600, "leakyRelu");
+	nn.addLayer(400, "leakyRelu");
+	nn.addLayer(200, "leakyRelu");
+	nn.addLayer(10, "linear");
 	nn.set_output(output, d_output);
-	CrossEntropy<double> crossEntropy(2, output, d_output, true);
+	CrossEntropy<double> crossEntropy(10, output, d_output, true);
 
 	//nn.random();
 	nn.loadParam("./nn");
+	
+	/*
+	for (size_t j = 0; j < testData.size(); j++) {
+		nn.forward(testData[j].first);
+		crossEntropy.forward();
+		crossEntropy.set_ans(testData[j].second);
+		if (!crossEntropy.isPredictionCorrect()) {
+			std::cout << j << " ";
+		}
+	}*/
 
 	
-	double test[2] = { 0.88,0.89 };
-	nn.forward(test);
-	crossEntropy.forward();
-	std::cout << "input     : {" << test[0] << ", " << test[1] << "}\n";
-	std::cout << "predict   : " << crossEntropy.prediction() << "\n";
-	std::cout << "confidence: " << crossEntropy.confidence() << "\n";
-	std::cout << "ans       : " << (test[0] > test[1] ? 0:1) <<"\n";
-	return 0;
-	int epock = 100;
+	for (;;) {
+		int ID;
+		std::cin >> ID;
+		if (ID >= testData.size()) {
+			continue;
+		}
+		mnistData.show(ID);
+		nn.forward(testData[ID].first);
+		crossEntropy.forward();
+		crossEntropy.set_ans(testData[ID].second);
+		std::cout << "predict   :" << crossEntropy.prediction() << "\n";
+		std::cout << "confidence:" << crossEntropy.confidence() << "\n";
+		std::cout << "ans       :" << testData[ID].second << "\n";
+		std::cout << "\n";
+	}
+	int batchSize = 1000;
+	int epock = 10000;
 	for (int i = 0; i < epock; i++) {
 		double loss = 0.;
-		int numOfCorrect = 0;
-		for (int j = 0; j < n; j++) {
-			nn.forward(x[j]);
+		std::vector<std::pair<double*, size_t>> trainData = mnistData.get_MiniBatchData((size_t)batchSize);
+		int numOfCorrect=0;
+		for (int j = 0; j < batchSize; j++) {
+			nn.forward(trainData[j].first);
 			crossEntropy.forward();
-			crossEntropy.set_ans((size_t)y[j]);
+			crossEntropy.set_ans(trainData[j].second);
 			loss += crossEntropy.calcLoss();
 			crossEntropy.backward();
 			nn.backward();
 			if (crossEntropy.isPredictionCorrect())numOfCorrect++;
 		}
 		std::cout << "loss    :" << loss << "\n";
-		std::cout << "accuracy:" << (double)numOfCorrect/(double)n << "\n";
-		nn.learn(0.1 / (double)n);
+		std::cout << "accuracy:" << (double)numOfCorrect/(double)batchSize << "\n";
+		nn.learn(0.01 / (double)batchSize);
+
+		if (i % 100 == 99) {
+			std::cout << "========================================================\n";
+			std::cout << "test\n";
+			numOfCorrect = 0;
+			for (size_t j = 0; j < testData.size(); j++) {
+				nn.forward(testData[j].first);
+				crossEntropy.forward();
+				crossEntropy.set_ans(testData[j].second);
+				if (crossEntropy.isPredictionCorrect())numOfCorrect++;
+			}
+
+			std::cout << "loss    :" << loss << "\n";
+			std::cout << "accuracy:" << (double)numOfCorrect / (double)testData.size() << "\n";
+			std::cout << "========================================================\n";
+			nn.saveParam("./nn");
+		}
 	}
-	
 	nn.saveParam("./nn");
 
 }
